4ec72c0a4de1142aa37070aaef732bba1c104c4e
Vulfixisme <124507539+Vulfixisme@users.noreply.github.com>
2024-03-04 18:37:26 +0700

    Add files via upload

---
+++ Tugas Constructor/src/DriverKucing.java
@@ -0,0 +1,8 @@
+public class DriverKucing {
+    public static void main(String[] args) {
+    Kucing deklarasi = new Kucing();
+    deklarasi.Deklarasi();
+    deklarasi.Deklarasi2();
+
+    }
+}
---
+++ Tugas Constructor/src/Kucing.java
@@ -0,0 +1,19 @@
+public class Kucing {
+    public String hewan = "kucing";
+    public String nama ="Vulfix";
+    public int umur = 1 ;
+    public String jeniskelamin = "Betina";
+    public String Jeniskucing ="Anggora";
+    public String kebiasaan ="tidur dan mencakar benda terdekat ";
+
+    public void Deklarasi(){
+        System.out.println("hewan :"+ hewan);
+        System.out.println("Nama :"+ nama);
+    }
+    public void Deklarasi2(){
+        System.out.println("Umur :"+ umur+ "tahun");
+        System.out.println("Jenis kelamin :"+  jeniskelamin);
+        System.out.println("Jenis kucing"+Jeniskucing);
+        System.out.println("Kebiasaan:"+kebiasaan);
+    }
+}
